9925972016faa8cce75da2c5ead62b8607c1f206
Scott Barr <scottbarr@gmail.com>
2014-04-11 17:35:40 +0100

    Update controllers.js

--- static/components/controllers.js
+++ static/components/controllers.js
@@ -10,9 +10,9 @@ function BusServiceController($scope, $routeParams, $http, StopModel) {
     $http.get('/api/' + STOP_CODE).success(function(data) {
         var spanWidth = parseInt(12 / data.services.length);
         $scope.spanWidth = spanWidth < 3 ? 3 : spanWidth;
-        $scope.stopModel = StopModel
-        $scope.stopModel.buses = data.services
-        $scope.stopModel.stopName = data.stop_info.stop_name
-        $scope.stopModel.stopCode = data.stop_info.stop_code
+        $scope.stopModel = StopModel;
+        $scope.stopModel.buses = data.services;
+        $scope.stopModel.stopName = data.stop_info.stop_name;
+        $scope.stopModel.stopCode = data.stop_info.stop_code;
     });
 };
